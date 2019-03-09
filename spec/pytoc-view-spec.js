49bb3106a67c07cd397954f5e571e710da2a5eb8
Dave Standish <david.standish@machine-uprising.com>
2019-03-09 00:28:11 -0300

    Initial design commit
    First design of the package. Still needs a fair bit of work

---
+++ spec/pytoc-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomFileTocView from '../lib/atom-file-toc-view';
+
+describe('AtomFileTocView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
